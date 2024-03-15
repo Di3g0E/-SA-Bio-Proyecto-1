75d229d4c2c10107353cb52181d36e2cd6b0965a
Kaiser_AI <josewalter2468@gmail.com>
2024-03-15 15:26:56 +0100

    mejor versión

--- main.py
+++ main.py
@@ -8,37 +8,34 @@ x, y = mnist['data'], mnist['target']
 
 df_x = pd.DataFrame(x)
 df_y = pd.DataFrame(y)
-#print(df)
-
-
-# Definir una función para convertir una fila en una lista de columnas
-def row_to_columns(row):
-    return [row[i:i+28] for i in range(0, len(row), 28)]
-
-# Aplicar la función a cada fila y crear una nueva columna con listas de columnas
-df_x_columns = df_x.apply(row_to_columns, axis=1)
 
 # Obtener la mitad de las filas
-mitad_filas = len(df_x_columns) // 2
+mitad_filas = len(df_x) // 2
 
 # Dividir el DataFrame a la mitad
-df_x_columns_1 = df_x_columns.iloc[:mitad_filas]
-df_x_columns_2 = df_x_columns.iloc[mitad_filas:]
+df_x_columns_1 = df_x.iloc[:mitad_filas].copy()
+df_x_columns_2 = df_x.iloc[mitad_filas:].copy()
 
 df_y_1 = df_y.iloc[:mitad_filas]
 df_y_2 = df_y.iloc[mitad_filas:]
 
-df_y_1.columns = ['target_1']
-df_y_2.columns = ['target_2']
+# Convertir las filas en listas y asignarlas a una nueva columna
+df_x_columns_1['x1'] = df_x_columns_1.apply(lambda row: row.tolist(), axis=1)
+df_x_columns_2['x2'] = df_x_columns_2.apply(lambda row: row.tolist(), axis=1)
 
-# Junto los cuatro en un solo data frame
-merged_df = pd.concat([df_x_columns_1.reset_index(drop=True),
-                       df_x_columns_2.reset_index(drop=True),
-                       df_y_1.reset_index(drop=True),
-                       df_y_2.reset_index(drop=True)], axis=1)
 
-# Agregar una nueva columna con 1 si los valores son iguales, 0 si son distintos
-merged_df['igualdad'] = merged_df.apply(lambda row: 1 if row['target_1'] == row['target_2'] else 0, axis=1)
+# Eliminar las columnas originales
+df_x_columns_1.drop(df_x_columns_1.columns[:-1], axis=1, inplace=True)
+df_x_columns_2.drop(df_x_columns_2.columns[:-1], axis=1, inplace=True)
 
-# Mostrar el DataFrame con la nueva columna
-print(merged_df.head())
+# Resetear los índices del DataFrame df_x_columns_2
+df_x_columns_2.reset_index(drop=True, inplace=True)
+df_y_2.reset_index(drop=True, inplace=True)
+
+# Concatenación del dataframe
+result_concat_col = pd.concat([df_x_columns_1, df_x_columns_2, df_y_1, df_y_2], axis=1)
+
+result_concat_col.columns.values[2:4] = ['y1', 'y2']
+result_concat_col['son_iguales'] = (result_concat_col['y1'] == result_concat_col['y2'])
+
+print(result_concat_col.head())
